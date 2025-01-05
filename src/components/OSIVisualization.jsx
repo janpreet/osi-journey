f2b8754b00ae619c940fb4f4c937509cc4aeeccd
Janpreet Singh <janpreetsinghgill@gmail.com>
2025-01-04 22:00:28 -0500

    toolttip position

--- src/components/OSIVisualization.jsx
+++ src/components/OSIVisualization.jsx
@@ -209,8 +209,7 @@ const OSIVisualization = () => {
                       {layer.id}. {layer.name}
                     </span>
                     {/* Tooltip */}
-                    <div className="opacity-0 group-hover:opacity-100 transition-opacity fixed transform -translate-y-1/2 left-1/3 p-3 bg-gray-900 text-white text-sm rounded-lg w-72 z-50 pointer-events-none shadow-xl">
-                      {layer.description}
+                    <div className="opacity-0 group-hover:opacity-100 transition-opacity absolute left-full top-1/2 ml-2 -translate-y-1/2 p-3 bg-gray-900 text-white text-sm rounded-lg w-72 z-50 pointer-events-none shadow-xl">                      {layer.description}
                     </div>
                   </div>
                 ))}
