281279f1cf32cbaa8eebaa4957cc37eb98f3a859
Janpreet Singh <janpreetsinghgill@gmail.com>
2025-01-04 21:30:30 -0500

    whoops

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
-import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/osi-journey/',
   plugins: [react()],
+  base: '/osi-journey/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
     },
   },
 })
